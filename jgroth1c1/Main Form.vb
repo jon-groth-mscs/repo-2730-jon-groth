d8941359b5c4554bb786a322a363adb4add47eac
tripl <tripl@DESKTOP-0HIPL2H>
2016-08-31 20:48:09 -0500

    Added ex1c

---
+++ jgroth1c1/Main Form.vb
@@ -0,0 +1,16 @@
+﻿Public Class MainForm
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        frmStarWars.Show()
+        tmrExit.Enabled = False
+        'Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        frmToucan.Show()
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Close()
+    End Sub
+End Class
---
+++ jgroth1c1/Star Wars.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmStarWars
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+End Class
