2d0f1d2b2dbcae2e9f580a55c5771473b62773ac
tripl <tripl@DESKTOP-0HIPL2H>
2016-10-05 18:48:19 -0500

    updated ex2a

--- jgroth2a1/Main Form.vb
+++ jgroth2a1/Main Form.vb
@@ -10,10 +10,11 @@ Option Strict On
 Public Class frmMain
 
     'class-level variable for storing salesclerk's name
-    Private strClerk As String
+    'Private strClerk As String
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
         ' calculate number of items sold and total sales
-
+        Const strPROMPT As String = "Salesclerk's Name:"
+        Const strTITLE As String = "Name Entry"
         Const decITEM_PRICE As Decimal = 0.5D
         Const decTAX_RATE As Decimal = 0.02D
 
@@ -23,6 +24,9 @@ Public Class frmMain
         Dim decSubtotal As Decimal
         Dim decSalesTax As Decimal
         Dim decTotalSales As Decimal
+        Static strClerk As String
+        'assign name to class-level variable
+        strClerk = InputBox(strPROMPT, strTITLE, strClerk)
 
         'calculate total number of items sold
         Integer.TryParse(txtDonuts.Text, intDonuts)
@@ -71,10 +75,16 @@ Public Class frmMain
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
         'get the salesclerk's name
 
-        Const strPROMPT As String = "Salesclerk's Name:"
-        Const strTITLE As String = "Name Entry"
-        'assign name to class-level variable
-        strClerk = InputBox(strPROMPT, strTITLE)
 
+
+    End Sub
+
+    Private Sub ClearLabels(sender As Object, e As EventArgs) _
+            Handles txtDonuts.TextChanged, txtMuffins.TextChanged
+        'clear the total items, total sales, and message
+
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        lblMsg.Text = String.Empty
     End Sub
 End Class
