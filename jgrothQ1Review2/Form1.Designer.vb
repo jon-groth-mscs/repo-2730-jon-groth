b53b717d34f2df7cb69db01aa564dbe8e4291a93
tripl <tripl@DESKTOP-0HIPL2H>
2016-10-04 19:44:33 -0500

    updated ex2a

---
+++ jgrothQ1Review2/Form1.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lblNet = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblState = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblFICA = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblFWT = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblGross = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPayRate = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(295, 161)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 28
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(213, 161)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 27
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(131, 161)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 26
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lblNet
+        '
+        Me.lblNet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNet.Location = New System.Drawing.Point(401, 109)
+        Me.lblNet.Name = "lblNet"
+        Me.lblNet.Size = New System.Drawing.Size(79, 23)
+        Me.lblNet.TabIndex = 38
+        Me.lblNet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(404, 92)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(27, 13)
+        Me.Label8.TabIndex = 37
+        Me.Label8.Text = "Net:"
+        '
+        'lblState
+        '
+        Me.lblState.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblState.Location = New System.Drawing.Point(308, 109)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(79, 23)
+        Me.lblState.TabIndex = 36
+        Me.lblState.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(310, 92)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 13)
+        Me.Label7.TabIndex = 35
+        Me.Label7.Text = "State:"
+        '
+        'lblFICA
+        '
+        Me.lblFICA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFICA.Location = New System.Drawing.Point(219, 109)
+        Me.lblFICA.Name = "lblFICA"
+        Me.lblFICA.Size = New System.Drawing.Size(79, 23)
+        Me.lblFICA.TabIndex = 34
+        Me.lblFICA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(222, 92)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(33, 13)
+        Me.Label6.TabIndex = 33
+        Me.Label6.Text = "FICA:"
+        '
+        'lblFWT
+        '
+        Me.lblFWT.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFWT.Location = New System.Drawing.Point(126, 109)
+        Me.lblFWT.Name = "lblFWT"
+        Me.lblFWT.Size = New System.Drawing.Size(79, 23)
+        Me.lblFWT.TabIndex = 32
+        Me.lblFWT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(130, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(34, 13)
+        Me.Label5.TabIndex = 31
+        Me.Label5.Text = "FWT:"
+        '
+        'lblGross
+        '
+        Me.lblGross.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGross.Location = New System.Drawing.Point(34, 109)
+        Me.lblGross.Name = "lblGross"
+        Me.lblGross.Size = New System.Drawing.Size(79, 23)
+        Me.lblGross.TabIndex = 30
+        Me.lblGross.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 29
+        Me.Label4.Text = "Gross:"
+        '
+        'txtPayRate
+        '
+        Me.txtPayRate.Location = New System.Drawing.Point(421, 53)
+        Me.txtPayRate.Name = "txtPayRate"
+        Me.txtPayRate.Size = New System.Drawing.Size(70, 20)
+        Me.txtPayRate.TabIndex = 25
+        Me.txtPayRate.Text = "0.00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(418, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 24
+        Me.Label3.Text = "Pay &Rate"
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(351, 54)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(46, 20)
+        Me.txtHours.TabIndex = 23
+        Me.txtHours.Text = "0.00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(348, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "&Hours:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(108, 54)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(231, 20)
+        Me.txtName.TabIndex = 21
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(108, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "&Name:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 76)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 19
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(526, 205)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblNet)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblFICA)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblFWT)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblGross)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtPayRate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents lblNet As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblState As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblFICA As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblFWT As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblGross As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtPayRate As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtHours As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ jgrothQ1Review2/Form1.vb
@@ -0,0 +1,25 @@
+﻿' COMC2730 Quiz 1 Part 2 Review
+' Jon Groth
+
+Option Strict On
+
+Public Class Form1
+
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'txtName.Text = "MsgA"
+        'Label6.Text = "MsgB"
+        lblNet.Text = Convert.ToString(Convert.ToInt32(txtHours.Text) + 10)
+        'lblNet.Text = Convert.ToString(Convert.ToInt32(txtHours.text) + PictureBox1.Top) 
+        'PictureBox1.Top = 100
+        'PictureBox1.Top = PictureBox1.Top + Convert.ToInt32(txtHours.Text)
+        'txtName.Text = "MsgC" + "MsgD"
+        'txtName.Text = "MsgC" & "MsgD"
+        'txtName.Text = "Gross Pay: " _
+        '    + Convert.ToString(Convert.ToInt32(txtHours.Text) + Convert.ToDouble(txtPayRate.Text))
+        'lblNet.Text = Format(1000, "N2")
+        'lblNet.Text = 1000.ToString("N2")
+        'lblNet.Text = (1000).ToString("N2")
+        'txtName.Text = "MsgD: " + (Convert.ToDouble(txtPayRate.text) * 100).ToString("N2")
+    End Sub
+End Class
